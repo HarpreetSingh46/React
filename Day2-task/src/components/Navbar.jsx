4e1202ad9321ad26fce68fc0657ea76ca4240c7a
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-01-29 18:58:38 +0530

    Day2-task

---
+++ Day2-task/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <div>
+          <h1>Horizon</h1>
+        </div>
+        <div className='Info'>
+          <h4><span className='span'>About</span></h4>
+          <h4>Services</h4>
+          <h4>Coaches</h4>
+          <h4>Events</h4>
+        </div>
+        <div className='Book'>
+          <h4>Book now</h4>
+        </div>
+  
+      </div>
+  )
+}
+
+export default Navbar
---
+++ Day2-task/src/components/Section.jsx
@@ -0,0 +1,16 @@
+import Center from './Center'
+const Section = () => {
+  return (
+    <div className='sec'>
+        {/* <img src="https://images.unsplash.com/photo-1603145369500-f9f476a0cb8e?q=80&w=1750&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /> */}
+    
+        <h1>Unplash your inner champion today.</h1>
+        <h1>All In One Place</h1>      
+        <p>join the ultimate tennis experience - where passion meets performance <br></br>  <span className='spantext'> and every swing bring  you closer to victory</span> </p>
+        <button>Start your own journey</button>
+    </div>
+    
+  )
+}
+
+export default Section
