e30d1e8e6ff419d4f737a2aa2fb17876a91e98d3
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-02-02 21:39:24 +0530

    usestate

--- Day5-hooks/src/App.jsx
+++ Day5-hooks/src/App.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
- const [king, setking] = useState("harpreet")
- const [Queen, setQueen] = useState("jashan")
+  const [num, setnum] = useState(0)
+//  const [king, setking] = useState("harpreet")
+//  const [Queen, setQueen] = useState("jashan")
 
- const changeking = ()=>{
-  setking("happy")
- }
- const changeQueen = ()=>{
-  setQueen("Tania")
- }
-  let a=0
+//  const changeking = ()=>{
+//   setking("happy")
+//  }
+//  const changeQueen = ()=>{
+//   setQueen("Tania")
+//  }
+  // let a=0
   return (
     <div>
-      <h2>{king}</h2>
+      {/* <h2>{king}</h2>
       <h3>{Queen}</h3>
       <button onClick={changeking}>king</button>
       <button onClick={changeQueen}>Queen</button>
@@ -30,9 +31,20 @@ const App = () => {
         console.log("btn clicked")
         let h1=document.querySelector("h1")
         h1.innerHTML=a
-      }}>decrease</button>
+        // }}>decrease</button> */   
+        }
+      
+      <div className="box">
+          {num}
+      </div>
+      <button onClick={()=>{
+        const num =Math.floor(Math.random()*100)
+        setnum(num)
+      }}>Click me</button>
+  
     </div>
   )
 }
 
 export default App
+ 
