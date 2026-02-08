258d0f30204a2f1da398ec8f93be240b772b6ab0
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-02-08 21:59:44 +0530

    Axios

--- Day9-Axios/src/App.jsx
+++ Day9-Axios/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios  from 'axios'
+import Card  from './components/Card'
 const App = () => {
+  const [alldata, setalldata] = useState([])
  async function getdata(){
-   const respones = await axios.get("https://jsonplaceholder.typicode.com/todos/100")
+   const respones = await axios.get("https://picsum.photos/v2/list")
+    setalldata(respones.data)
     console.log(respones)
   }
   return (
     <div>
       <button onClick={getdata}>Get data</button>
+      <div className='card'>
+          {alldata.map(function(elem,idx){
+        return  <div key={idx}><Card elem={elem} /></div>
+      }
+    )}
+      </div>
     </div>
   )
 }
