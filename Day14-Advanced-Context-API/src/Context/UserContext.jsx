7d67563cdb4e3471ec22bb6e9e4291c0681bec8c
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-02-13 21:07:52 +0530

    updated=Context-Api

--- Day14-Advanced-Context-API/src/Context/UserContext.jsx
+++ Day14-Advanced-Context-API/src/Context/UserContext.jsx
@@ -1,10 +1,53 @@
 import React, { createContext } from 'react'
 export const UserDataContext = createContext()
 const UserContext = (props) => {
-    const user='Harpreet'
+   const users = [
+  {
+    id: 1,
+    name: "Aarav Sharma",
+    email: "aarav.sharma@example.com",
+    age: 24,
+    role: "Frontend Developer",
+    city: "Delhi"
+  },
+  {
+    id: 2,
+    name: "Meera Iyer",
+    email: "meera.iyer@example.com",
+    age: 27,
+    role: "UI/UX Designer",
+    city: "Bengaluru"
+  },
+  {
+    id: 3,
+    name: "Rohan Verma",
+    email: "rohan.verma@example.com",
+    age: 22,
+    role: "Backend Developer",
+    city: "Pune"
+  },
+  {
+    id: 4,
+    name: "Sneha Patel",
+    email: "sneha.patel@example.com",
+    age: 29,
+    role: "Project Manager",
+    city: "Ahmedabad"
+  },
+  {
+    id: 5,
+    name: "Kabir Khan",
+    email: "kabir.khan@example.com",
+    age: 26,
+    role: "Full Stack Developer",
+    city: "Mumbai"
+  }
+];
+
+
   return (
     <div>
-      <UserDataContext.Provider value={user}>
+      <UserDataContext.Provider value={users}>
         {props.children}
       </UserDataContext.Provider>
     </div>
--- Day14-Advanced-Context-API/src/components/Section.jsx
+++ Day14-Advanced-Context-API/src/components/Section.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import UserContext, { UserDataContext } from '../Context/UserContext'
 
 const Section = () => {
+  const data = UserContext(UserDataContext)
+  console.log(data);
+  
   return (
     <div className='h-90 bg-zinc-900'>
       <h1>All section </h1>
